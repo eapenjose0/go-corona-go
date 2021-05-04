b68152de82c96d9bb7e4a24436f9b38ad1819986
Eapen Jose <eapenjose0@gmail.com>
2021-05-04 13:29:50 +0530

    venv setup done

--- main.py
+++ main.py
@@ -19,46 +19,50 @@ def get_district_list():
 
 
 def main():
-    district_list = get_district_list()
-    district_ids = district_list.keys()
+    try:
+        district_list = get_district_list()
+        district_ids = district_list.keys()
 
-    message_details_file = open("data/message_details.pkl", "rb")
-    prev_message_details = pickle.load(message_details_file)
-    
-    for district_id in district_ids:
-        # State code: 17 - Kerala
-        kerala_centers = Centers(state_code=STATE_CODE, search_n_days=SEARCH_N_DAYS).find_centers_by_district_id(district_id=district_id)
+        message_details_file = open("data/message_details.pkl", "rb")
+        prev_message_details = pickle.load(message_details_file)
         
-        DISTRICT_NAME = district_list[district_id]
-        final_message = ""
-        final_message += message_utils.get_header()
-        final_message  += f"\n\n"
-
-        # Delete previous message
-        if prev_message_details.get(district_id):
-            message_id = prev_message_details[district_id]["message_id"]
-            chat_id = prev_message_details[district_id]["chat_id"]
-
-            print(message_id, chat_id)
-            try:
-                telegram_utils.bot.delete_message(chat_id=chat_id,
-                message_id=message_id)
-            except Exception as e:
-                pass
-
-
-        if len(kerala_centers) == 0: 
-            continue         
-        
-        for center in kerala_centers:
-            message = message_utils.prepare_message(center)
-            final_message += message
+        for district_id in district_ids:
+            # State code: 17 - Kerala
+            kerala_centers = Centers(state_code=STATE_CODE, search_n_days=SEARCH_N_DAYS).find_centers_by_district_id(district_id=district_id)
             
-        prev_message_details[district_id] = telegram_utils.send_alert(final_message, district_id)
+            DISTRICT_NAME = district_list[district_id]
+            final_message = ""
+            final_message += message_utils.get_header()
+            final_message  += f"\n\n"
 
-    message_details_file = open("data/message_details.pkl", "wb")
-    pickle.dump(prev_message_details, message_details_file)
-    message_details_file.close()
+            # Delete previous message
+            if prev_message_details.get(district_id):
+                message_id = prev_message_details[district_id]["message_id"]
+                chat_id = prev_message_details[district_id]["chat_id"]
+
+                print(message_id, chat_id)
+                try:
+                    telegram_utils.bot.delete_message(chat_id=chat_id,
+                    message_id=message_id)
+                except Exception as e:
+                    pass
+
+
+            if len(kerala_centers) == 0: 
+                continue         
+            
+            for center in kerala_centers:
+                message = message_utils.prepare_message(center)
+                final_message += message
+                
+            prev_message_details[district_id] = telegram_utils.send_alert(final_message, district_id)
+
+        message_details_file = open("data/message_details.pkl", "wb")
+        pickle.dump(prev_message_details, message_details_file)
+        message_details_file.close()
+    except Exception as e:
+        print(f"Exception: {e}")
+        pass        
    
 
 if __name__ == '__main__':
